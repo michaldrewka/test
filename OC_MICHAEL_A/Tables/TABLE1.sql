13925752f130431fd39a6b916fb638505a37d7ad
Michal Drewka <drewka@tkomp.pl>
2018-12-17 10:09:52 +0100

    br1 change1

--- OC_MICHAEL_A/Tables/TABLE1.sql
+++ OC_MICHAEL_A/Tables/TABLE1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE oc_michael_a.table1 (
   col1 NUMBER(10) NOT NULL,
   col2 NUMBER(10),
+  col3 NUMBER(10),
   CONSTRAINT col1_pk PRIMARY KEY (col1)
 );
 COMMENT ON COLUMN oc_michael_a.table1.col1 IS 'this is a comment';
