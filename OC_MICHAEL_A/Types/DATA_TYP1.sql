ca79505a16451599894b50fcb8cc904cc34f6d29
michaldrewka <michal.drewka@gmail.com>
2020-08-26 14:33:54 +0200

    clear

--- OC_MICHAEL_A/Types/DATA_TYP1.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TYPE oc_michael_a.data_typ1 AS OBJECT
-(
-    data1 NUMBER,
-    MEMBER FUNCTION proc(increment NUMBER) RETURN NUMBER
-);
-/
--- OC_MICHAEL_A/Types/TYPE1.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TYPE oc_michael_a.type1 AS OBJECT (
-  id           NUMBER,
-  field1  VARCHAR2(50),
-  field2  NUMBER
-);
-/
